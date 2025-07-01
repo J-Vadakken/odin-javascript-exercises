53f1802fdda2e2f07274cd451f504cc8c9f95469
jgv555 <jgvadakken5@gmail.com>
2025-07-01 16:00:17 -0400

    Finish Exercise 12 and 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((tot, val) => tot+val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((val, cur_val) => val*cur_val, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let arr = [1];
+  let i = 2;
+  while (i<=a) {
+    arr.push(i);
+    i++;
+  }
+  return arr.reduce((val, i) => val*i, 1);
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuation = [" ", ",", ".", "!"]
+    let str_p = string;
+    for (mark of punctuation) {
+        str_p = str_p.split(mark).join("");
+    }
+    console.log(str_p);
+    str_p = str_p.toLowerCase();
+    let arr = str_p.split("");
+    let i = 0;
+    while (i < arr.length) {
+        if (arr[i] != arr[arr.length-1-i]) return false;
+        i++;
+    }
+    return true;
 };
 
 // Do not edit below this line
