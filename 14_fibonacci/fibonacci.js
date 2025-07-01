15973ae558d60fbea76f0b5788b66cdf9bdbffc4
jgv555 <jgvadakken5@gmail.com>
2025-07-01 16:09:22 -0400

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a = 1;
+    let b = 1;
+    let c = 2;
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n <= 2) return 1;
+    let i = 2;
+    while (i<n) {
+        c = a + b;
+        b = a;
+        a = c;
+        i++;
+    }
+    return a;
 };
 
 // Do not edit below this line
