01a140e8bf0f2a397496b64e5e0fdcf8be662ec8
jgv555 <jgvadakken5@gmail.com>
2025-07-01 16:15:47 -0400

    Finish exercise 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,8 +1,9 @@
 const getTheTitles = function(arr_obj) {
-    return arr_obj.reduce((arr, val) => {
-        arr.push(val.title);
-        return arr;
-    },[]);
+    // return arr_obj.reduce((arr, val) => {
+    //     arr.push(val.title);
+    //     return arr;
+    // },[]);
+    return arr_obj.map(val => val.title);
 };
 
 // Do not edit below this line
