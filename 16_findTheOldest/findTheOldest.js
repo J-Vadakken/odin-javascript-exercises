d84bb66ddbbf05dd4890ae1c14d23cfc5c7785d7
jgv555 <jgvadakken5@gmail.com>
2025-07-01 16:33:35 -0400

    Finish exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr_obj) {
+    return arr_obj.reduce((oldest, person) => {
+        let swap_flag = false;
+        const currentDate = new Date();
 
+        let old_dod = oldest.yearOfDeath;
+        if (old_dod == undefined) {
+            old_dod = currentDate.getFullYear();
+        }
+        let person_dod = person.yearOfDeath;
+        if (person_dod == undefined) {
+            person_dod = currentDate.getFullYear();
+        }
+        let old_dob = oldest.yearOfBirth;
+        let person_dob = person.yearOfBirth;
+        swap_flag = (old_dod - old_dob) <= (person_dod - person_dob);
+        
+        return swap_flag ? person : oldest;
+    })
 };
 
 // Do not edit below this line
